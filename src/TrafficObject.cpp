f4363588674d83af8244234e182f104b285750b3
Taha Pourjalali <tpourjalali@gmail.com>
2020-04-08 20:13:53 +0000

    Solution1

---
+++ src/TrafficObject.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <chrono>
+#include "TrafficObject.h"
+
+// init static variable
+int TrafficObject::_idCnt = 0;
+
+void TrafficObject::setPosition(double x, double y)
+{
+    _posX = x;
+    _posY = y;
+}
+
+void TrafficObject::getPosition(double &x, double &y)
+{
+    x = _posX;
+    y = _posY;
+}
+
+TrafficObject::TrafficObject()
+{
+    _type = ObjectType::noObject;
+    _id = _idCnt++;
+}
+
+TrafficObject::~TrafficObject()
+{
+    for(auto& t: _threads)
+        t.join();
+}
